e0277063133e2d628c8eac89085e8097375766d6
Diana Krapivina <mr.ikoma@mail.ru>
2021-06-02 21:00:57 +0300

    Create tests

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
-    void should() {
+    void shouldPostToPostman() {
         // Given - When - Then
 // Предусловия
         given()
@@ -20,6 +20,40 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldCurrentDate() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("02.06.21") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("02.06.21"))
+        ;
+    }
+
+    @Test
+    void shouldCurrentDat() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency:RUB") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency:USD"))
         ;
     }
 }
